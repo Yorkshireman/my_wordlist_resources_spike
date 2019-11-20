c39ac9e2004e01542fba3c78fb08b392ddd5a98f
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-11-20 00:05:18 +0000

    create POST /wordlists route to create wordlists

--- app/models/wordlist.rb
+++ app/models/wordlist.rb
@@ -2,5 +2,4 @@ class Wordlist < ApplicationRecord
   has_many :wordlist_entries
   has_many :words, through: :wordlist_entries
   validates :user_id, presence: true
-  validates_format_of :user_id, with: /[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/
 end
