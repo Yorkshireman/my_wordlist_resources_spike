55d2968fdba52fda213fdc9375973374fcc2fc1b
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-11-21 01:25:28 +0000

    get wordlist

--- app/controllers/wordlists_controller.rb
+++ app/controllers/wordlists_controller.rb
@@ -2,7 +2,6 @@ require_relative '../helpers/token_helper'
 
 class WordlistsController < ApplicationController
   include TokenHelper
-
   def create
     token = request.headers['Authorization'].split(' ').last
     user_id = decode_token(token)[0]['user_id']
@@ -16,5 +15,24 @@ class WordlistsController < ApplicationController
         }
       }
     end
+  end
+
+  def show
+    token = request.headers['Authorization'].split(' ').last
+    wordlist_id = decode_token(token)[0]['wordlist_id']
+    wordlist = JSON.parse(Wordlist.find(wordlist_id).to_json).symbolize_keys
+    token = generate_token({ exp: (Time.now + 1800).to_i, wordlist_id: wordlist[:id] })
+    render json: {
+      data: {
+        token: token,
+        type: 'wordlists',
+        id: wordlist_id,
+        attributes: {
+          created_at: wordlist[:created_at],
+          updated_at: wordlist[:updated_at],
+          user_id: wordlist[:user_id]
+        }
+      }
+    }
   end
 end
