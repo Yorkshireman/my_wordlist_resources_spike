032f16356cda54b92699dd6b04f02eca987a6350
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-11-26 23:46:19 +0000

    fetch wordlist by user_id or wordlist_id

--- app/controllers/wordlists_controller.rb
+++ app/controllers/wordlists_controller.rb
@@ -19,20 +19,23 @@ class WordlistsController < ApplicationController
 
   def show
     token = request.headers['Authorization'].split(' ').last
-    wordlist_id = decode_token(token)[0]['wordlist_id']
-    wordlist = JSON.parse(Wordlist.find(wordlist_id).to_json).symbolize_keys
-    token = generate_token({ exp: (Time.now + 1800).to_i, wordlist_id: wordlist[:id] })
-    render json: {
-      data: {
-        token: token,
-        type: 'wordlists',
-        id: wordlist_id,
-        attributes: {
-          created_at: wordlist[:created_at],
-          updated_at: wordlist[:updated_at],
-          user_id: wordlist[:user_id]
+    decode_token(token)[0].then do |decoded_token|
+      id = decoded_token['user_id'] || decoded_token['wordlist_id']
+      wordlist = JSON.parse(Wordlist.find(id).to_json).symbolize_keys
+      generate_token({ exp: (Time.now + 1800).to_i, wordlist_id: wordlist[:id] }).then do |token|
+        render json: {
+          data: {
+            token: token,
+            type: 'wordlists',
+            id: wordlist[:id],
+            attributes: {
+              created_at: wordlist[:created_at],
+              updated_at: wordlist[:updated_at],
+              user_id: wordlist[:user_id]
+            }
+          }
         }
-      }
-    }
+      end
+    end
   end
 end
